04339702936c80b46f276d8c9412a3259db96bc6
1031066280@qq.com <1031066280@qq.com>
2016-01-16 17:46:44 +0800

    柳伟杰提交

--- app/src/main/java/liuliu/quzhang/ui/activity/MainActivity.java
+++ app/src/main/java/liuliu/quzhang/ui/activity/MainActivity.java
@@ -3,6 +3,7 @@ package liuliu.quzhang.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -53,7 +54,7 @@ public class MainActivity extends BaseActivity {
         mList.add(new YZXXModel("1301020100001", true, "40合同章", "金属章体", "新刻印章", "石家庄市佳美网络科技有限责任公司", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2016-01-11 11:26:15.000", false));
         mList.add(new YZXXModel("1301020100002", true, "40X30发票专用章", "塑料章体", "新刻印章", "石家庄市佳美网络科技有限责任公司", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-11-22 14:27:32.000", false));
         mList.add(new YZXXModel("1301020100003", true, "38财务专用章", "金属章体", "新刻印章", "石家庄市佳美网络科技有限责任公司", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-10-29 15:23:41.000", false));
-        mList.add(new YZXXModel("1301020100004", true, "20X20法人名章", "金属章体", "新刻印章", "石家庄市佳美网络科技有限责任公司", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-9-11 09:28:55.000", false));
+        mList.add(new YZXXModel("1301020100004", true, "20X20法人名章", "金属章体", "新刻印章", "石家庄市佳美网络科技有限责任公司", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-09-11 09:28:55.000", false));
         mList.add(new YZXXModel("1301020100005", true, "40行政章", "塑料章体", "新刻印章", "河北创维网络科技有限公司", "王博", "河北创维网络科技有限公司", "长安区印章刻制中心", "2015-08-14 11:23:11.000", false));
         mList.add(new YZXXModel("1301020100006", true, "40X30发票专用章", "金属章体", "新刻印章", "河北创维网络科技有限公司", "王博", "河北创维网络科技有限公司", "长安区印章刻制中心", "2015-09-11 08:25:42.000", false));
         mList.add(new YZXXModel("1301020100007", true, "40行政章", "金属章体", "新刻印章", "河北创维网络科技有限公司的比较常用的章", "王博", "河北创维网络科技有限公司", "长安区印章刻制中心", "2016-01-12 08:51:22.000", false));
@@ -149,7 +150,8 @@ public class MainActivity extends BaseActivity {
                         }
                     });
                 } else {
-                    holder.setVisible(R.id.quzhang_btn_main, false);
+                    Button btn = holder.getView(R.id.quzhang_btn_main);
+                    btn.setText("已取章");
                 }
 
             }
