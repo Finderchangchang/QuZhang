093c7642c956f5b5fc7af667b53fc14fd1b6d8e3
1031066280@qq.com <1031066280@qq.com>
2016-01-18 09:31:18 +0800

    柳伟杰提交

--- app/src/main/java/liuliu/quzhang/ui/activity/MainActivity.java
+++ app/src/main/java/liuliu/quzhang/ui/activity/MainActivity.java
@@ -51,23 +51,22 @@ public class MainActivity extends BaseActivity {
      * 加载数据到数据库
      */
     private void initDatas() {
-        mList.add(new YZXXModel("1301020100001", true, "40合同章", "金属章体", "新刻印章", "石家庄市佳美网络科技有限责任公司", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2016-01-11 11:26:15.000", "0","胡海珍","13145673214"));
-        mList.add(new YZXXModel("1301020100002", true, "40X30发票专用章", "塑料章体", "新刻印章", "石家庄市佳美网络科技有限责任公司", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-11-22 14:27:32.000", "0","胡海珍","13145673214"));
-        mList.add(new YZXXModel("1301020100003", true, "38财务专用章", "金属章体", "新刻印章", "石家庄市佳美网络科技有限责任公司", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-10-29 15:23:41.000","0","胡海珍","13145673214"));
-        mList.add(new YZXXModel("1301020100004", true, "20X20法人名章", "金属章体", "新刻印章", "石家庄市佳美网络科技有限责任公司", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-09-11 09:28:55.000", "0","王枫林","18915673214"));
-        mList.add(new YZXXModel("1301020100005", true, "40行政章", "塑料章体", "新刻印章", "河北创维网络科技有限公司", "王博", "河北创维网络科技有限公司", "长安区印章刻制中心", "2015-08-14 11:23:11.000", "0","胡海珍","13145673214"));
-        mList.add(new YZXXModel("1301020100006", true, "40X30发票专用章", "金属章体", "新刻印章", "河北创维网络科技有限公司", "王博", "河北创维网络科技有限公司", "长安区印章刻制中心", "2015-09-11 08:25:42.000", "0","王枫林","18915673214"));
-        mList.add(new YZXXModel("1301020100007", true, "40行政章", "金属章体", "新刻印章", "河北创维网络科技有限公司的比较常用的章", "王博", "河北创维网络科技有限公司", "长安区印章刻制中心", "2016-01-12 08:51:22.000", "0","王枫林","18915673214"));
-        mList.add(new YZXXModel("1301020100008", true, "20X20法人名章", "塑料章体", "新刻印章", "石家庄市佳美网络科技有限责任公司的比较常用的章", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-03-21 16:22:35.000", "0","胡海珍","13145673214"));
-        mList.add(new YZXXModel("1301020100009", true, "40合同章", "金属章体", "新刻印章", "石家庄市佳美网络科技有限责任公司的比较常用的章", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-03-25 13:27:35.000", "0","胡海珍","13145673214"));
-        mList.add(new YZXXModel("1301020100010", true, "38财务专用章", "塑料章体", "新刻印章", "石家庄市佳美网络科技有限责任公司的比较常用的章", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-04-29 09:27:32.000", "0","王枫林","18915673214"));
+        mList.add(new YZXXModel("1301020100001", true, "40合同章", "金属章体", "新刻印章", "石家庄市佳美网络科技有限责任公司", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2016-01-11 11:26:15.000", "0", "胡海珍", "13145673214"));
+        mList.add(new YZXXModel("1301020100002", true, "40X30发票专用章", "塑料章体", "新刻印章", "石家庄市佳美网络科技有限责任公司", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-11-22 14:27:32.000", "0", "胡海珍", "13145673214"));
+        mList.add(new YZXXModel("1301020100003", true, "38财务专用章", "金属章体", "新刻印章", "石家庄市佳美网络科技有限责任公司", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-10-29 15:23:41.000", "0", "胡海珍", "13145673214"));
+        mList.add(new YZXXModel("1301020100004", true, "20X20法人名章", "金属章体", "新刻印章", "石家庄市佳美网络科技有限责任公司", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-09-11 09:28:55.000", "0", "王枫林", "18915673214"));
+        mList.add(new YZXXModel("1301020100005", true, "40行政章", "塑料章体", "新刻印章", "河北创维网络科技有限公司", "王博", "河北创维网络科技有限公司", "长安区印章刻制中心", "2015-08-14 11:23:11.000", "0", "胡海珍", "13145673214"));
+        mList.add(new YZXXModel("1301020100006", true, "40X30发票专用章", "金属章体", "新刻印章", "河北创维网络科技有限公司", "王博", "河北创维网络科技有限公司", "长安区印章刻制中心", "2015-09-11 08:25:42.000", "0", "王枫林", "18915673214"));
+        mList.add(new YZXXModel("1301020100007", true, "40行政章", "金属章体", "新刻印章", "河北创维网络科技有限公司的比较常用的章", "王博", "河北创维网络科技有限公司", "长安区印章刻制中心", "2016-01-12 08:51:22.000", "0", "王枫林", "18915673214"));
+        mList.add(new YZXXModel("1301020100008", true, "20X20法人名章", "塑料章体", "新刻印章", "石家庄市佳美网络科技有限责任公司的比较常用的章", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-03-21 16:22:35.000", "0", "胡海珍", "13145673214"));
+        mList.add(new YZXXModel("1301020100009", true, "40合同章", "金属章体", "新刻印章", "石家庄市佳美网络科技有限责任公司的比较常用的章", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-03-25 13:27:35.000", "0", "胡海珍", "13145673214"));
+        mList.add(new YZXXModel("1301020100010", true, "38财务专用章", "塑料章体", "新刻印章", "石家庄市佳美网络科技有限责任公司的比较常用的章", "郑斌文", "石家庄市佳美网络科技有限责任公司", "长安区印章刻制中心", "2015-04-29 09:27:32.000", "0", "王枫林", "18915673214"));
     }
 
     @Override
     public void initEvents() {
-        List mResult = null;
         try {
-            mResult = mDB.findAll(YZXXModel.class);
+            List mResult = mDB.findAll(YZXXModel.class);
             if (mResult.size() == 0) {
                 for (YZXXModel model : mList) {
                     mDB.save(model);
@@ -78,7 +77,7 @@ public class MainActivity extends BaseActivity {
                 mDB.save(model);
             }
         }
-        reloadList(mResult);
+        reloadList(mList);
 
     }
 
@@ -98,11 +97,10 @@ public class MainActivity extends BaseActivity {
     // 回调方法，从第二个页面回来的时候会执行这个方法
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
+        String choice = data.getStringExtra("choice").toString().trim();
         // 根据上面发送过去的请求吗来区别
         switch (requestCode) {
             case 0:
-                String choice = data.getStringExtra("choice").toString().trim();
                 if (!choice.equals("")) {
                     search_choice_tv.setText("条件：“" + choice + "”");
                     List<YZXXModel> list = mDB.findAllByWhere(YZXXModel.class, "SignetId like '%" + choice + "%' or UserCompanyName like '%" + choice + "%'");
@@ -141,9 +139,7 @@ public class MainActivity extends BaseActivity {
                 holder.setText(R.id.yz_content_tv, model.getYZContent());
                 holder.setText(R.id.jb_name_tv, model.getJBName());
                 holder.setText(R.id.kz_unit_tv, model.getKZUnit());
-                if (!model.getQUZhang().equals("1")) {
-                    Button btn = holder.getView(R.id.quzhang_btn_main);
-                    btn.setText("取章");
+                if (model.getQUZhang().equals("0")) {
                     holder.setOnClickListener(R.id.quzhang_btn_main, new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
